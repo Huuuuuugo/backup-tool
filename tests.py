e6e82520e4bea4543aa4cae9c6b59074137c612d
Hugo Messias <hugomessias9@gmail.com>
2025-01-29 11:50:54 -0300

    Create test for when a break contition is met on the same cycle as a file ends

--- tests.py
+++ tests.py
@@ -34,10 +34,27 @@ class TempFileHelper:
 
 class TestCore:
     def test_get_changes_with_label_bth_once(self):
-        """Test a byte sequence where both an addition and a deletion occur in the exact same cycle once"""
+        """Test a byte sequence where both an addition and a deletion occur in the exact same cycle once."""
         with TempFileHelper() as helper:
             old_file_path = helper.create(b"this is an example of very short a file")
             new_file_path = helper.create(b"this is an example from a file")
+
+            changes = get_changes(old_file_path, new_file_path)
+            apply_changes(changes, old_file_path)
+
+            with open(old_file_path, "rb") as old_file:
+                with open(new_file_path, "rb") as new_file:
+                    assert old_file.read() == new_file.read()
+
+    def test_get_changes_with_break_condition_and_empty_file_on_same_cycle(self):
+        """Test a byte sequence where one of the break conditions is met on the exact same cycle as a file ends.
+
+        This should prioritize the break contition, as that is what will result on the actual change being saved.
+        If this is not the case, an empty change will be saved instead of the real change.
+        """
+        with TempFileHelper() as helper:
+            old_file_path = helper.create(b"new file with some extra content and this")
+            new_file_path = helper.create(b"old content with a whole whole lot of removed contentaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 
             changes = get_changes(old_file_path, new_file_path)
             apply_changes(changes, old_file_path)
